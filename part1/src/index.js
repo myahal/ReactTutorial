40c83aac930a78004f8326a5b254f0fd80c55112
Chiharu Miya <miya.chiharu@gmail.com>
2020-02-16 18:54:24 +0900

    use destructuring for part1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({name, age}) => {
+  const bornYear = () => {
+    return new Date().getFullYear() - age
+  }
+
     return(
         <div>
-        <p>Hello World {props.name}, you are {props.age} years old</p>
+        <p>Hello World {name}, you are {age} years old</p>
+        <p>So you were probably born in {bornYear()}</p>
         </div>
     )
 }
 
-const Footer = () => {
-    return (
-      <div>
-        greeting app created by 
-        <a href="https://github.com/mluukkai">mluukkai</a>
-      </div>
-    )
-  }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
 
-    return(
-        <div>
-            <h1>Greeting</h1>
-            <Hello name="George" age={20}/>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
