260ae7c10ddf978f64b8130b0d82b35043a69f73
myahal <miya.chiharu@gmail.com>
2020-02-18 17:00:16 +0900

    useStateを使用して、ノートの追加やフィルタ機能を追加

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
 
-const App = ({ notes }) => {
-  const rows = () => notes.map(note =>
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const rows = () => notesToShow.map(note =>
     <Note key={note.id} note={note} />)
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {rows()}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
