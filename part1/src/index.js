a42edf40022e8f15dd2a60c62528b89c8d8a9b02
myahal <miya.chiharu@gmail.com>
2020-02-18 16:10:49 +0900

    update for part2 example

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const Hello = ({name, age}) => {
-  const bornYear = () => {
-    return new Date().getFullYear() - age
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
   }
-
-    return(
-        <div>
-        <p>Hello World {name}, you are {age} years old</p>
-        <p>So you were probably born in {bornYear()}</p>
-        </div>
-    )
-}
+]
 
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
