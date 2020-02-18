99d9c488e289811a4b540add04e750d0074120a4
myahal <miya.chiharu@gmail.com>
2020-02-18 18:00:02 +0900

    get from server via axios

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const rows = () => notesToShow.map(note =>
     <Note key={note.id} note={note} />)
