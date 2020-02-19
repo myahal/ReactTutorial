4b609258ffbd426b484adc484f3553eab17c6448
myahal <miya.chiharu@gmail.com>
2020-02-19 18:06:40 +0900

    person backend

---
+++ person-backend/index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
+    {
+        "name": "harry",
+        "number": "12345",
+        "id": 5
+    },
+    {
+        "name": "taro",
+        "number": "aaaa",
+        "id": 6
+    },
+    {
+        "name": "hanako",
+        "number": "1234",
+        "id": 7
+    }
+]
+
+
+app.get('/info', (req, res) => {
+    const now = new Date()
+    res.send(`<div>Phone book info for ${persons.length} people</div><div>${now.toISOString()}</div>`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find((p) => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter((p) => p.id !== id)
+    res.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * (1000 - persons.length)) + persons.length
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons = persons.concat(person)
+    res.json(person)
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
