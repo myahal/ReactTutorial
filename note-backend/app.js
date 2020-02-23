1d73afc334fa04c2fde14a9ee1ec3bfdee41269b
Chiharu Miya <miya.chiharu@gmail.com>
2020-02-23 11:16:03 +0900

    first deploy

--- note-backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-const notesRouter = require('./controllers/notes')
-app.use('/api/notes', notesRouter)
-
-module.exports = app
